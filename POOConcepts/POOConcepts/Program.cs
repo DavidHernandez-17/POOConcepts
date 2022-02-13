56ca617a3167b4af408344d1bff1b1ca0fe4d816
LUIS DAVID HERNANDEZ GIRALDO <luishernandez298106@correo.itm.edu.co>
2022-02-13 12:47:19 -0500

    Finish date class.

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("_____________");
+
+try
+{
+    Console.WriteLine(new Date(2024, 02, 29));
+    Console.WriteLine(new Date(1992, 2, 7));
+    Console.WriteLine(new Date(1985, 11, 31));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
